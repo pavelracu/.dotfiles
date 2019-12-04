ad9a54ceae939aee3bf3e24779d3d74512adac0d
Pavel Racu <pavel.racu@gmail.com>
2019-12-04 01:16:10 +0000

    Add new pull, push, container restart etc aliases.

--- .aliases.sh
+++ .aliases.sh
@@ -4,9 +4,9 @@ alias ltr='ls -lthr'
 
 ## Custom git aliases
 alias gs='git status'
-alias gbv='git branch -v'
-alias push='git push origin $1'
-alias pull='git pull origin $1'
+alias gbv='git branch --format="%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(color:blue)%(authorname)%(color:reset) (%(color:green)%(committerdate:relative)%(color:reset))"'
+alias pull='git pull origin `git rev-parse --abbrev-ref HEAD`'
+alias push='git push origin `git rev-parse --abbrev-ref HEAD`'
 
 # DOCKER related aliases
 alias dbuild='$(docker_build)'
@@ -16,6 +16,9 @@ alias ddebug='docker-compose -f docker-compose.yml -f docker-compose.local.debug
 alias ddown='docker-compose down --remove-orphans'
 alias drm='docker rm $(docker ps -a -q)'
 alias dvls='docker volume ls'
+
+# Container restart
+alias cr='docker-compose up -d --no-deps --build '
 
 ## Custom cd to dev directories
 alias cdb='cd $DEV_HOME/district-backend'
