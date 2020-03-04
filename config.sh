8225c137db565c04328531a294f5a34ff0983f8f
Pavel Racu <pavel.racu@gmail.com>
2020-03-04 17:20:52 +0000

    Add new commands to config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,23 @@
-sudo pacman -S -y git vim maven zsh powerline fonts-powerline
+sudo pacman -S -y git vim maven zsh snapd
+
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+sudo snap install code --classic
+sudo snap install postman teams-for-linux
+sudo snap install android-studio --classic
+sudo snap install intellij-idea-ultimate --classic
+
+sudo pacman -S docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo usermod -aG docker $USER
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Update the source listing
 sudo pacman -Syy
-#apt-get update Ensure that you have the binaries needed to fetch repo listing
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-# Fetch the repository listing from docker's site & add it
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# Update source listing now that we've added Docker's repo
-sudo pacman -Syy
-sudo pacman -S docker-ce=18.06.2~ce~3-0~ubuntu
-sudo usermod -aG docker $USER
 
 git config --global user.email "pavel.racu@gmail.com"
 git config --global user.name "Pavel Racu"
