21cc89a4a7786325a018ff8778e82b0b2fe9cea9
Pavel Racu <pavel.racu@gmail.com>
2020-04-02 11:39:06 +0100

    Add git hooks config

--- config.sh
+++ config.sh
@@ -3,10 +3,10 @@ sudo apt install -y git vim maven zsh node snap
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
-sudo snap install code --classic
-sudo snap install postman
-sudo snap install android-studio --classic
-sudo snap install intellij-idea-ultimate --classic
+#sudo snap install code --classic
+#sudo snap install postman
+#sudo snap install android-studio --classic
+#sudo snap install intellij-idea-ultimate --classic
 
 ## install docker
 sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
@@ -34,3 +34,5 @@ git config --global user.email "pavel.racu@gmail.com"
 git config --global user.name "Pavel Racu"
 
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+git config core.hooksPath ~/.dotfiles/hooks
